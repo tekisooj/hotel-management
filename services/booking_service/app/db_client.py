7e649fba030d2176a20c284dc7d64fe3968d4e2e
tekisooj <72854262+tekisooj@users.noreply.github.com>
2025-09-20 13:59:09 +0200

    fix booking db

--- services/booking_service/app/db_client.py
+++ services/booking_service/app/db_client.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 import json
 from uuid import UUID
@@ -8,27 +9,24 @@ from sqlalchemy import and_, create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from models import Base, BookingDB
 
-class HotelManagementDBClient:
-    def __init__(
-            self,  hotel_management_database_secret_name: str | None, region: str
-    ) -> None:
 
+class HotelManagementDBClient:
+    def __init__(self, hotel_management_database_secret_name: str | None, region: str) -> None:
         if not hotel_management_database_secret_name:
             raise ValueError("Secret name must be provided or set in environment variables.")
-        
+
         self.db_url = self.build_db_url_from_secret(hotel_management_database_secret_name, region)
-        
+
         if not self.db_url:
             raise ValueError("Unable to get db url.")
 
-        
         self.engine = create_engine(self.db_url)
         self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
         Base.metadata.create_all(bind=self.engine)
 
     def get_secret_by_name(self, secret_name: str, region_name: str = "us-east-1") -> dict:
         client = boto3.client("secretsmanager", region_name=region_name)
-        
+
         try:
             response = client.get_secret_value(SecretId=secret_name)
             secret_string = response["SecretString"]
@@ -41,46 +39,45 @@ class HotelManagementDBClient:
     def build_db_url_from_secret(self, secret_name: str, region: str) -> str:
         secret = self.get_secret_by_name(secret_name, region)
 
-        username = str(secret.get('username', '')).strip()
-        password = str(secret.get('password', '')).strip()
-        host = str(secret.get('host', '')).strip()
-        port = str(secret.get('port', '')).strip()
-        dbname = str(secret.get('dbname', '')).strip()
+        username = str(secret.get("username", "")).strip()
+        password = str(secret.get("password", "")).strip()
+        port = str(secret.get("port", "")).strip()
+        dbname = str(secret.get("dbname", "")).strip()
+
+        proxy_endpoint = os.getenv("DB_PROXY_ENDPOINT")
+        if proxy_endpoint:
+            host = proxy_endpoint
+        else:
+            host = str(secret.get("host", "")).strip()
 
         if not all([username, password, host, port, dbname]):
-            raise ValueError('Database secret is missing required fields.')
+            raise ValueError("Database secret is missing required fields.")
 
-        return (
-            f"postgresql+psycopg2://{username}:{password}"
-            f"@{host}:{port}/{dbname}"
-        )
+        return f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{dbname}"
 
     def get_session(self) -> Session:
         return self.SessionLocal()
-    
+
     def add_booking(self, booking: Booking) -> UUID:
         session = self.get_session()
-
         try:
             booking_obj = BookingDB(
                 user_uuid=booking.user_uuid,
                 room_uuid=booking.room_uuid,
-                check_in = booking.check_in,
-                check_out = booking.check_out,
+                check_in=booking.check_in,
+                check_out=booking.check_out,
                 total_price=booking.total_price,
-                status=booking.status             
+                status=booking.status,
             )
             session.add(booking_obj)
             session.commit()
             session.refresh(booking_obj)
-
             return Booking.model_validate(booking_obj).uuid
         finally:
             session.close()
 
     def get_booking(self, booking_uuid: UUID) -> Booking | None:
         session = self.get_session()
-
         try:
             booking = session.query(BookingDB).filter(BookingDB.uuid == booking_uuid).first()
             if not booking:
@@ -89,14 +86,13 @@ class HotelManagementDBClient:
         finally:
             session.close()
 
-    
     def get_filtered_bookings(
         self,
         user_uuid: UUID | None = None,
         room_uuid: UUID | None = None,
         status: str | None = None,
         check_in: datetime | None = None,
-        check_out: datetime | None = None
+        check_out: datetime | None = None,
     ) -> list[Booking]:
         session = self.get_session()
         try:
@@ -112,7 +108,7 @@ class HotelManagementDBClient:
                 query = query.filter(
                     and_(
                         BookingDB.check_in <= check_out,
-                        BookingDB.check_out >= check_in
+                        BookingDB.check_out >= check_in,
                     )
                 )
 
@@ -120,7 +116,7 @@ class HotelManagementDBClient:
             return [Booking.model_validate(b) for b in bookings]
         finally:
             session.close()
-    
+
     def update_booking(self, update_request: BookingUpdateRequest) -> Booking:
         session = self.get_session()
         try:
@@ -130,13 +126,10 @@ class HotelManagementDBClient:
 
             if update_request.check_in:
                 setattr(booking, "check_in", update_request.check_in)
-
             if update_request.check_out:
                 setattr(booking, "check_out", update_request.check_out)
-
             if update_request.total_price:
                 setattr(booking, "total_price", update_request.total_price)
-
             if update_request.status:
                 setattr(booking, "status", update_request.status)
 
@@ -152,8 +145,8 @@ class HotelManagementDBClient:
             booking = session.query(BookingDB).filter(BookingDB.uuid == booking_uuid).first()
             if not booking:
                 raise ValueError("Booking not found")
-            
-            booking.status = BookingStatus.CANCELLED # type: ignore
+
+            booking.status = BookingStatus.CANCELLED  # type: ignore
             session.commit()
             session.refresh(booking)
             return Booking.model_validate(booking)
@@ -167,13 +160,9 @@ class HotelManagementDBClient:
                 BookingDB.room_uuid == room_uuid,
                 BookingDB.status != "canceled",
                 BookingDB.check_in < check_out,
-                BookingDB.check_out > check_in
+                BookingDB.check_out > check_in,
             ).first()
 
             return overlapping_booking_exists is None
         finally:
             session.close()
-
-
-
-
--- services/booking_service/infra/api_stack.py
+++ services/booking_service/infra/api_stack.py
@@ -1,13 +1,20 @@
 from aws_cdk import (
     Stack,
     Duration,
+    CfnOutput
 )
 from aws_cdk.aws_lambda import Function, Runtime, Code
 from aws_cdk.aws_apigateway import RestApi, LambdaIntegration, EndpointType
 from aws_cdk.aws_iam import Role, ServicePrincipal, ManagedPolicy
 from aws_cdk.aws_secretsmanager import Secret
 from constructs import Construct
-from aws_cdk.aws_ec2 import Vpc, SecurityGroup, SubnetSelection, SubnetType
+from aws_cdk.aws_ec2 import Vpc, SecurityGroup, SubnetSelection, SubnetType, Port
+from aws_cdk.aws_rds import (
+    DatabaseInstance,
+    DatabaseInstanceAttributes,
+    DatabaseProxy,
+    ProxyTarget
+)
 
 class BookingServiceStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, env_name: str, pr_number: str | None = None, **kwargs):
@@ -16,40 +23,65 @@ class BookingServiceStack(Stack):
 
         vpc = Vpc.from_lookup(self, "HotelManagementVpc", vpc_id="vpc-00688d23d81374471")
 
-        security_group = SecurityGroup.from_security_group_id(
+        db_sg = SecurityGroup.from_security_group_id(
             self, "HotelManagementDbSG",
             security_group_id="sg-018c80394ac5a0590"
         )
 
         lambda_role = Role(
             self, f"BookingServiceLambdaRole-{env_name}{f'-{pr_number}' if pr_number else ''}",
-            assumed_by=ServicePrincipal("lambda.amazonaws.com"), # type: ignore
+            assumed_by=ServicePrincipal("lambda.amazonaws.com"),
             managed_policies=[
                 ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),
                 ManagedPolicy.from_aws_managed_policy_name("AmazonRDSDataFullAccess"),
                 ManagedPolicy.from_aws_managed_policy_name("SecretsManagerReadWrite"),
                 ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaVPCAccessExecutionRole")
-
             ]
-            
         )
 
-        secret_name = f"hotel-management-database-{self.env_name if self.env_name == 'prod' else 'int'}"
+        db_name = f"hotel-management-database-{self.env_name if self.env_name == 'prod' else 'int'}"
+        db_secret = Secret.from_secret_name_v2(self, "DbSecret", secret_name=db_name)
+
+        if self.env_name=="prod":
+            db_endpoint="hotel-management-database-prod.cluster-capkwmowwxnt.us-east-1.rds.amazonaws.com"
+        else:
+            db_endpoint="hotel-management-database-int.cluster-capkwmowwxnt.us-east-1.rds.amazonaws.com"
+
+        db_instance = DatabaseInstance.from_database_instance_attributes(
+            self, "ImportedDbInstance",
+            instance_endpoint_address=f"{db_name}.cluster-xxxxxx.us-east-1.rds.amazonaws.com",
+            instance_identifier=db_endpoint,
+            port=5432,
+            security_groups=[db_sg]
+        )
+
+        db_proxy = DatabaseProxy(
+            self, f"BookingDbProxy-{env_name}{f'-{pr_number}' if pr_number else ''}",
+            proxy_target=ProxyTarget.from_instance(db_instance),
+            vpc=vpc,
+            secrets=[db_secret],
+            require_tls=True,
+            idle_client_timeout=Duration.minutes(30),
+            max_connections_percent=50,
+            vpc_subnets=SubnetSelection(subnet_type=SubnetType.PRIVATE_WITH_EGRESS),
+            security_groups=[db_sg]
+        )
 
         lambda_function = Function(
             self, f"BookingServiceFunction-{env_name}{f'-{pr_number}' if pr_number else ''}",
             runtime=Runtime.PYTHON_3_11,
             handler="main.handler",
             code=Code.from_asset("services/booking_service/app"),
-            role=lambda_role, # type: ignore
+            role=lambda_role,
             timeout=Duration.seconds(30),
             memory_size=512,
             environment={
                 "BOOKING_SERVICE_ENV": self.env_name,
-                "HOTEL_MANAGEMENT_DATABASE_SECRET_NAME": secret_name,
+                "HOTEL_MANAGEMENT_DATABASE_SECRET_NAME": db_name,
+                "DB_PROXY_ENDPOINT": db_proxy.endpoint, 
             },
             vpc=vpc,
-            security_groups=[security_group],
+            security_groups=[db_sg],
             vpc_subnets=SubnetSelection(
                 subnet_type=SubnetType.PRIVATE_WITH_EGRESS
             )
@@ -61,7 +93,7 @@ class BookingServiceStack(Stack):
             description="API Gateway exposing booking service Lambda",
             endpoint_types=[EndpointType.REGIONAL],
         )
-        integration = LambdaIntegration(lambda_function, proxy=True) # typing: ignore
+        integration = LambdaIntegration(lambda_function, proxy=True)
 
         resource_booking = api.root.add_resource("booking")
         resource_booking.add_method("POST", integration)
@@ -78,3 +110,5 @@ class BookingServiceStack(Stack):
 
         resource_availability = api.root.add_resource("availability")
         resource_availability.add_method("GET", integration)
+
+        CfnOutput(self, "DbProxyEndpoint", value=db_proxy.endpoint)
