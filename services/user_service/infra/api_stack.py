7aff37e7dd518202cef5baa601f48fc5a4a0c20a
tekisooj <72854262+tekisooj@users.noreply.github.com>
2025-09-27 16:49:11 +0200

    fix

--- services/user_service/infra/api_stack.py
+++ services/user_service/infra/api_stack.py
@@ -4,19 +4,44 @@ from aws_cdk import (
     CfnOutput
 )
 from aws_cdk.aws_lambda import Function, Runtime, Code
-from aws_cdk.aws_apigateway import (
-    RestApi,
-    LambdaIntegration,
-    EndpointType,
-    MockIntegration,
-    IntegrationResponse,
-    MethodResponse,
-    PassthroughBehavior
-)
+from aws_cdk.aws_apigateway import RestApi, LambdaIntegration, EndpointType, MockIntegration, IntegrationResponse, MethodResponse, PassthroughBehavior
 from aws_cdk.aws_iam import Role, ServicePrincipal, ManagedPolicy
 from aws_cdk.aws_secretsmanager import Secret
 from constructs import Construct
 from aws_cdk.aws_ec2 import Vpc, SecurityGroup, SubnetSelection, SubnetType
+
+
+def add_cors_options(resource):
+    """
+    Adds a CORS preflight OPTIONS method to the given API Gateway resource.
+    """
+    resource.add_method(
+        "OPTIONS",
+        MockIntegration(
+            integration_responses=[
+                IntegrationResponse(
+                    status_code="200",
+                    response_parameters={
+                        "method.response.header.Access-Control-Allow-Headers": "'*'",
+                        "method.response.header.Access-Control-Allow-Origin": "'*'",
+                        "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,GET,POST,DELETE,PATCH'",
+                    },
+                )
+            ],
+            passthrough_behavior=PassthroughBehavior.WHEN_NO_MATCH,
+            request_templates={"application/json": '{"statusCode": 200}'},
+        ),
+        method_responses=[
+            MethodResponse(
+                status_code="200",
+                response_parameters={
+                    "method.response.header.Access-Control-Allow-Headers": True,
+                    "method.response.header.Access-Control-Allow-Methods": True,
+                    "method.response.header.Access-Control-Allow-Origin": True,
+                },
+            )
+        ],
+    )
 
 
 class UserServiceStack(Stack):
@@ -33,7 +58,7 @@ class UserServiceStack(Stack):
 
         lambda_role = Role(
             self, f"UserServiceLambdaRole-{env_name}{f'-{pr_number}' if pr_number else ''}",
-            assumed_by=ServicePrincipal("lambda.amazonaws.com"),  # type: ignore
+            assumed_by=ServicePrincipal("lambda.amazonaws.com"),
             managed_policies=[
                 ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),
                 ManagedPolicy.from_aws_managed_policy_name("AmazonRDSDataFullAccess"),
@@ -47,33 +72,28 @@ class UserServiceStack(Stack):
 
         if self.env_name == "prod":
             proxy_role_arn = "arn:aws:iam::914242301564:role/service-role/rds-proxy-role-1758401519769"
+            proxy_endpoint = "hotel-management-db-proxy-prod.proxy-capkwmowwxnt.us-east-1.rds.amazonaws.com"
+            user_pool_id = "us-east-1_Wtvh2rdSQ"
+            audience = "7226gqpnghn0q22ec2ill399lv"
+            app_client_id = "7226gqpnghn0q22ec2ill399lv"
         else:
             proxy_role_arn = "arn:aws:iam::914242301564:role/service-role/rds-proxy-role-1758401469124"
+            proxy_endpoint = "hotel-management-db-proxy-int.proxy-capkwmowwxnt.us-east-1.rds.amazonaws.com"
+            user_pool_id = "us-east-1_DDS5D565p"
+            audience = "la13fgbn7avmni0f84pu5lk79"
+            app_client_id = "la13fgbn7avmni0f84pu5lk79"
 
         proxy_role = Role.from_role_arn(self, "ImportedProxyRole", proxy_role_arn)
         db_secret.grant_read(proxy_role)
 
-        if self.env_name == "prod":
-            proxy_endpoint = "hotel-management-db-proxy-prod.proxy-capkwmowwxnt.us-east-1.rds.amazonaws.com"
-        else:
-            proxy_endpoint = "hotel-management-db-proxy-int.proxy-capkwmowwxnt.us-east-1.rds.amazonaws.com"
-
-        if self.env_name == "prod":
-            user_pool_id = "us-east-1_Wtvh2rdSQ"
-            audience = "7226gqpnghn0q22ec2ill399lv"
-        else:
-            user_pool_id = "us-east-1_DDS5D565p"
-            audience = "la13fgbn7avmni0f84pu5lk79"
-
         jwks_url = f"https://cognito-idp.us-east-1.amazonaws.com/{user_pool_id}/.well-known/jwks.json"
-        app_client_id = "7226gqpnghn0q22ec2ill399lv" if self.env_name == "prod" else "la13fgbn7avmni0f84pu5lk79"
 
         lambda_function = Function(
             self, f"UserServiceFunction-{env_name}{f'-{pr_number}' if pr_number else ''}",
             runtime=Runtime.PYTHON_3_11,
             handler="main.handler",
             code=Code.from_asset("services/user_service/app"),
-            role=lambda_role,  # type: ignore
+            role=lambda_role,
             timeout=Duration.seconds(30),
             memory_size=1024,
             environment={
@@ -96,46 +116,18 @@ class UserServiceStack(Stack):
             endpoint_types=[EndpointType.REGIONAL],
         )
 
-        integration = LambdaIntegration(lambda_function, proxy=True)  # type: ignore
-
-        def add_cors_options(resource):
-            resource.add_method(
-                "OPTIONS",
-                MockIntegration(
-                    integration_responses=[
-                        IntegrationResponse(
-                            status_code="200",
-                            response_parameters={
-                                "method.response.header.Access-Control-Allow-Headers": "'Content-Type,Authorization'",
-                                "method.response.header.Access-Control-Allow-Origin": "'*'",
-                                "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,GET,POST,PATCH,DELETE'",
-                            },
-                        )
-                    ],
-                    passthrough_behavior=PassthroughBehavior.WHEN_NO_MATCH,
-                    request_templates={"application/json": '{"statusCode": 200}'},
-                ),
-                method_responses=[
-                    MethodResponse(
-                        status_code="200",
-                        response_parameters={
-                            "method.response.header.Access-Control-Allow-Headers": True,
-                            "method.response.header.Access-Control-Allow-Origin": True,
-                            "method.response.header.Access-Control-Allow-Methods": True,
-                        },
-                    )
-                ],
-            )
+        integration = LambdaIntegration(lambda_function, proxy=True)
 
         resource_me = api.root.add_resource("me")
+        resource_user = api.root.add_resource("user")
+        resource_user_id = resource_user.add_resource("{user_uuid}")
+
         resource_me.add_method("GET", integration)
         add_cors_options(resource_me)
 
-        resource_user = api.root.add_resource("user")
         resource_user.add_method("POST", integration)
         add_cors_options(resource_user)
 
-        resource_user_id = resource_user.add_resource("{user_uuid}")
         resource_user_id.add_method("GET", integration)
         resource_user_id.add_method("DELETE", integration)
         resource_user_id.add_method("PATCH", integration)
