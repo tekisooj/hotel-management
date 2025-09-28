e7c96154ec199359c062108039b1ae4655f9804a
tekisooj <72854262+tekisooj@users.noreply.github.com>
2025-09-28 03:39:28 +0200

    fix

--- services/test_service/infra/app.py
+++ services/test_service/infra/app.py
@@ -1,6 +1,12 @@
-from aws_cdk import App
+from aws_cdk import App, Environment
 from api_stack import TestServiceStack
 
 app = App()
-TestServiceStack(app, "TestServiceStack-int", env_name="int")
+
+# Define your AWS account and region explicitly
+env = Environment(account="914242301564", region="us-east-1")
+
+# Create the stack with the environment
+TestServiceStack(app, "TestServiceStack-int", env_name="int", env=env)
+
 app.synth()
