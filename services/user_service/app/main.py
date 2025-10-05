0d13bdffcad211f298d714e0544e94d30d0cfe1d
tekisooj <72854262+tekisooj@users.noreply.github.com>
2025-10-05 15:24:25 +0200

    int auth ui

--- services/user_service/app/main.py
+++ services/user_service/app/main.py
@@ -45,7 +45,7 @@ def create_app() -> FastAPI:
 
     # --- CORS ---
     allowed_origins = [
-        "https://dz5wjcpk0b8gl.cloudfront.net",  # INT Auth UI
+        "https://d157dmqdv1g4x6.cloudfront.net",  # INT Auth UI
         "https://d2ecdgwxri75mv.cloudfront.net",  # PROD Auth UI
         "https://demfm8bnd6dtk.cloudfront.net",   # Guest INT
         "https://dsfjwq83frzww.cloudfront.net",   # Guest PROD
