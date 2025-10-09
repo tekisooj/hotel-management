af8a7c8368f15966a737c89fd304138967c5dac1
tekisooj <72854262+tekisooj@users.noreply.github.com>
2025-10-09 03:38:58 +0200

    fix map

--- microfrontends/guest_ui/components/MiniMap.vue
+++ microfrontends/guest_ui/components/MiniMap.vue
@@ -1,4 +1,4 @@
-<template>
+﻿<template>
   <div ref="container" class="mini-map"></div>
 </template>
 
@@ -14,14 +14,40 @@ onMounted(() => {
   const maplibre = (window as any).maplibregl
   if (!maplibre?.Map) return
 
+  const style = {
+    version: 8,
+    sources: {
+      'osm-tiles': {
+        type: 'raster',
+        tiles: [
+          'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        ],
+        tileSize: 256,
+        attribution: '© OpenStreetMap contributors',
+      },
+    },
+    layers: [
+      {
+        id: 'osm-tiles',
+        type: 'raster',
+        source: 'osm-tiles',
+        minzoom: 0,
+        maxzoom: 19,
+      },
+    ],
+  }
+
   const map = new maplibre.Map({
     container: container.value,
-    style: 'https://demotiles.maplibre.org/style.json',
+    style,
     center: [props.longitude, props.latitude],
     zoom: props.zoom ?? 12,
     interactive: false,
   })
 
+  map.addControl(new maplibre.AttributionControl({ compact: true }))
   new maplibre.Marker().setLngLat([props.longitude, props.latitude]).addTo(map)
 })
 </script>
